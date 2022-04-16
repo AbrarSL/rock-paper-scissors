f0b19515c95e0fff1fe318990f7526ce41a14520
AbrarSL <abrarsl2002@gmail.com>
2022-04-16 21:20:31 +0530

    Add game() function
    This is the main game function that will coordinate and
    keep track of wins, losses and ties.

--- script.js
+++ script.js
@@ -17,8 +17,22 @@ function computerPlay() {
     }
 }
 
+function playerPlay() {
+    let playerSelection;
+
+    while (true) {
+        playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+
+        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+            break;
+        }
+    }
+
+    return playerSelection;
+}
+
 function playRound(playerSelection, computerSelection) {
-    let roundResult = [false, false, computerSelection];
+    let roundResult = [false, false];
 
     if (playerSelection === computerSelection) {
         roundResult[0] = true;
@@ -40,3 +54,43 @@ function playRound(playerSelection, computerSelection) {
 
     return roundResult;
 }
+
+function game(rounds) {
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+
+    for (let i = 0; i < rounds; i++) {
+        let resultString;
+
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result[0]) {
+            ties++;
+            resultString = `You Tied! You both played ${playerSelection}`;
+        } else if (result[1]) {
+            wins++;
+            resultString = `You Win! Your ${playerSelection} beat ${computerSelection}`;
+        } else {
+            losses++;
+            resultString = `You Lose! Your ${playerSelection} lost to ${computerSelection}`;
+        }
+
+        console.log(resultString);
+    }
+
+    if (wins > losses) {
+        console.log("You won!");
+    } else if (wins === losses) {
+        console.log("You tied!");
+    } else {
+        console.log("You lost!")
+    }
+
+    console.log(`Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
+}
+
+game(5);
