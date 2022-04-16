b854c4e7f83707f456589f2c8a3521e99207dbd2
AbrarSL <abrarsl2002@gmail.com>
2022-04-16 20:22:55 +0530

    Add computerPlay function
    This function will return a randomly chosen play.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function genRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
+
+function computerPlay() {
+    let randomChoiceNumber = genRandomNumber(1, 3);
+
+    switch (randomChoiceNumber) {
+        case (1):
+            return "Rock";
+
+        case (2):
+            return "Paper";
+
+        case (3):
+            return "Scissors";
+    }
+}
